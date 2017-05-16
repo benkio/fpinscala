22a8746f41754d7693d29e150a0250c13cbe4c89
benkio <benkio89@gmail.com>
2017-05-16 13:23:10 +0200

    dive into par library

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -17,22 +17,45 @@ object Par {
     def cancel(evenIfRunning: Boolean): Boolean = false 
   }
   
-  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = // `map2` doesn't evaluate the call to `f` in a separate logical thread, in accord with our design choice of having `fork` be the sole function in the API for controlling parallelism. We can always do `fork(map2(a,b)(f))` if we want the evaluation of `f` to occur in a separate thread.
+/*  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = // `map2` doesn't evaluate the call to `f` in a separate logical thread, in accord with our design choice of having `fork` be the sole function in the API for controlling parallelism. We can always do `fork(map2(a,b)(f))` if we want the evaluation of `f` to occur in a separate thread.
     (es: ExecutorService) => {
       val af = a(es) 
       val bf = b(es)
       UnitFuture(f(af.get, bf.get)) // This implementation of `map2` does _not_ respect timeouts, and eagerly waits for the returned futures. This means that even if you have passed in "forked" arguments, using this map2 on them will make them wait. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
     }
+ */
   
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
     es => es.submit(new Callable[A] { 
       def call = a(es).get
     })
 
+  def lazyUnit[A](a : A) = fork(unit(a))
+
+  /*
   def map[A,B](pa: Par[A])(f: A => B): Par[B] = 
     map2(pa, unit(()))((a,_) => f(a))
+   */
+
+  def asyncF[A, B](f : A => B) : A => Par[B] =
+    a => lazyUnit(f(a))
 
   def sortPar(parList: Par[List[Int]]) = map(parList)(_.sorted)
+
+  def product[A, B](fa : Par[A], fb : Par[B]) : Par[(A, B)] =
+    (e : ExecutorService) => UnitFuture((fa(e).get, fb(e).get))
+
+  def map[A, B](fa : Par[A])(f : A => B) : Par[B] =
+    (e : ExecutorService) => UnitFuture(f(fa(e).get))
+
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    map(product(a, b))(t => f(t._1, t._2))
+
+  def parMap[A, B](l : List[A])(f : A => B) : Par[List[B]] = l match {
+    case Nil     => unit(Nil)
+    case x :: xs => map2(unit(f(x)), parMap(xs)(f))(_ :: _)
+  }
+
 
   def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean = 
     p(e).get == p2(e).get
