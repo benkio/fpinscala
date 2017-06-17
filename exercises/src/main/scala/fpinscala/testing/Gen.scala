d3854b43bc09b67622f562c704f6eea03ec60a35
benkio <benkio89@gmail.com>
2017-06-17 15:02:04 +0200

    finished the section 8.3.4

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -15,20 +15,22 @@ shell, which you can fill in and modify while working through the chapter.
 */
 
 trait Status
-case object Proven extends Status
+case object Exhausted   extends Status
+case object Proven      extends Status
 case object Unfalsified extends Status
 
-case class Prop(run : (TestCases, RNG) => Result) {
-  def &&(p : Prop) : Prop = Prop{ (t, r) =>
-    if (run(t, r).isLeft) run(t, r) else p.run(t, r)
+
+case class Prop(run : (MaxSize, TestCases, RNG) => Result) {
+  def &&(p : Prop) : Prop = Prop{ (m, t, r) =>
+    if (run(m, t, r).isLeft) run(m, t, r) else p.run(m, t, r)
   }
 
-  def ||(p : Prop) : Prop = Prop{ (t, r) =>
-    if (run(t, r).isRight) run(t, r) else p.run(t, r)
+  def ||(p : Prop) : Prop = Prop{ (m, t, r) =>
+    if (run(m, t, r).isRight) run(m, t, r) else p.run(m, t, r)
   }
 
   def tag(msg : String) : Prop =
-    Prop{ (t, r) => run(t,r) match {
+    Prop{ (m, t, r) => run(m, t, r) match {
            case Left(s) => Left(msg + s)
            case x => x
          }
@@ -37,45 +39,70 @@ case class Prop(run : (TestCases, RNG) => Result) {
 
 object Prop {
   type TestCases    = Int
+  type MaxSize      = Int
+  type Size         = Int
   type FailedCase   = String
   type SuccessCount = Int
   type Result       = Either[FailedCase, (Status, SuccessCount)]
 
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = Prop {
-    (n, rng) => {
-      def go(i : Int, j : Int, s : Stream[Option[A]], onEnd : Int => Result) : Result =
+    (max, n,rng) => {
+      def go(i: Int, j: Int, s: Stream[Option[A]], onEnd : Int => Result): Result =
         if (i == j) Right((Unfalsified, i))
         else s match {
-          case Cons(h, t) => h() match {
-            case Some(h) =>  try { if (f(h)) go(i+1, j, s, onEnd) else Left(h.toString) }
-                             catch { case e : Exception => Left(buildMsg(h, e)) }
-            case None => Right((Unfalsified, i))
+          case Cons(h,t) => h() match {
+            case Some(h) =>
+              try {
+                if (f(h)) go(i+1,j,t(),onEnd)
+                else Left(h.toString) }
+              catch { case e: Exception => Left(buildMsg(h, e)) }
+            case None => Right((Unfalsified,i))
           }
-          case Empty => onEnd(i)
+          case _ => onEnd(i)
         }
-
-      go(0, n/3, gen.exaustive, i => Right((Proven, i))) match {
-        case Right((Unfalsified, _)) => {
+      val endResult : Int => Result =
+        if (n < max) i => Right((Proven,i))
+        else i => Right((Exhausted, i))
+      go(0, Math.min(n, max)/3, gen.exhaustive, endResult) match {
+        case Right((Unfalsified,_)) =>
           val rands = randomStream(gen)(rng).map(Some(_))
           go(n/3, n, rands, i => Right((Unfalsified, i)))
-        }
-        case s => s
+        case s => s // If proven or failed, stop immediately
       }
     }
   }
+
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop {
+    (max, n, rng) => {
+      val casesPerSize = n / max + 1
+      val props : List[Prop] =
+        Stream.from(0).take(max + 1).map(i => forAll(g(i))(f)).toList
+      props.map(p => Prop((max, n, rng) => p.run(max, casesPerSize, rng)))
+           .reduceLeft(_ && _).run(max, n, rng)
+    }
+  }
+
+  def forAll[A](g : Sized[A])(f : A => Boolean) : Prop =
+    forAll(g.forSize)(f)
 
   def buildMsg[A](s : A, e : Exception) : String =
     "test case " + s + "\n" +
     "generated an exception" + e.getMessage + "\n" +
     "stack trace:\n" + e.getStackTrace.mkString("\n")
+
+
+  def buildMsg[A](s : A) : String =
+    "test case " + s + "\n" +
+    "failed to pass the prop"
+
 }
 
 
-case class Gen[+A](sample: State[RNG, A], exaustive : Stream[Option[A]]) {
-  def map[B](f: A => B): Gen[B] = Gen(sample.map(f), exaustive.map(o => o.map(f)))
+case class Gen[+A](sample: State[RNG, A], exhaustive : Stream[Option[A]]) {
+  def map[B](f: A => B): Gen[B] = Gen(sample.map(f), exhaustive.map(o => o.map(f)))
   def map2[B, C](g : Gen[B])(f : (A, B) => C) : Gen[C] = Gen(
     this.sample.map2(g.sample)(f),
-    this.exaustive.zipWith(g.exaustive)((oa, ob) =>
+    this.exhaustive.zipWith(g.exhaustive)((oa, ob) =>
       for {
         a <- oa
         b <- ob
@@ -85,13 +112,13 @@ case class Gen[+A](sample: State[RNG, A], exaustive : Stream[Option[A]]) {
   def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(
     sample.flatMap(a => f(a).sample),
     Stream.empty
-    /*exaustive.flatMap {
+    /*exhaustive.flatMap {
       case None => Stream(None)
-      case Some(a) => f(a).exaustive
+      case Some(a) => f(a).exhaustive
      }*/
   )
 
-
+  def unsized : SGen[A] = Unsized(this)
 }
 
 object Gen {
@@ -139,9 +166,11 @@ object Gen {
     Stream.unfold(rng)(rng => Some(gen.sample.run(rng)))
 }
 
+trait SGen[+A]
+case class Sized[+A](forSize : Size => Gen[A]) extends SGen[A]
+case class Unsized[+A](get : Gen[A]) extends SGen[A]
 
-
-trait SGen[+A] {
-
+object SGen {
+  def listOfN[A](g : Gen[A]) : SGen[List[A]] =
+    Sized(size => Gen.listOfN(size, g))
 }
-
