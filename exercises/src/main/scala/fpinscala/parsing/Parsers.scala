b13b4089575c5518f61ea2464796f72586c4f41e
benkio <benkio89@gmail.com>
2017-06-23 15:25:46 +0200

    starting the chapter 9, the 8 was boring

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -4,9 +4,21 @@ import language.higherKinds
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
 
+  def char(c : Char) : Parser[Char]
+
+  def orA(s1 : A, s2 : A) : Parser[A]
+
+  def run[A](p : Parser[A])(input : String) : Either[ParserError, A]
+
+  def listOfN[A](n : Int, p : Parser[A]) : Parser[List[A]]
+
+  implicit def string(s : String) : Parser[String]
+  implicit def operators[A](p : Parser[A]) = ParserOps(p)
+  implicit def asStringParser[A](implicit f : A => Parser[String]) = ParserOps(f(a))
+
   case class ParserOps[A](p: Parser[A]) {
-
-
+    def |[B>:A](p2 : Parser[B]) : Parser[B] = self.orA(p, p2)
+    def or[B>:A](p2 : Parser[B]) : Parser[B] = self.orA(p, p2)
   }
 
   object Laws {
