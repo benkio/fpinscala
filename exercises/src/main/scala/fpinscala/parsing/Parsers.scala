768e06ffff3aa375532aec0ac00010bfe573d051
benkio <benkio89@gmail.com>
2017-08-08 16:21:12 +0200

    finished the section 9.2.1

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -12,24 +12,41 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
   def char(c : Char) : Parser[Char] =
     string(c.toString).map(_.charAt(0))
 
-  def or[A](s1 : Parser[A], s2 : Parser[A]) : Parser[A]
+  def or[A](s1 : Parser[A], s2 : => Parser[A]) : Parser[A]
 
   def run[A](p : Parser[A])(input : String) : Either[ParserError, A]
 
-  def listOfN[A](n : Int, p : Parser[A]) : Parser[List[A]]
+  def listOfN[A](n : Int, p : Parser[A]) : Parser[List[A]] =
+    if (n <= 0) succeed(List()) else map2(p, listOfN(n-1, p))(_ :: _)
 
-  def many[A](p : Parser[A]) : Parser[List[A]]
+  def many[A](p : Parser[A]) : Parser[List[A]] =
+    or( map2(p, many(p))((a, l) => a :: l ),
+        succeed(List()))
+
+  def many1[A](p : Parser[A]) : Parser[List[A]] =
+    map2(p, many(p))((a, la) => a :: la)
 
   def map[A, B](a : Parser[A])(f : A => B) : Parser[B]
 
   def succeed[A](a : A) : Parser[A] =
     string("").map(_ => a)
 
+  def slice[A](p : Parser[A]) : Parser[String]
+
+  def product[A, B](p1 : Parser[A], p2 : => Parser[B]) : Parser[(A, B)]
+
+  def map2[A, B, C](p1 : Parser[A], p2 : => Parser[B])(f : (A, B) => C) : Parser[C] =
+    map(product(p1, p2))(f.tupled)
+
   case class ParserOps[A](p: Parser[A]) {
     def |[B>:A](p2 : Parser[B]) : Parser[B] = self.or(p, p2)
     def or[B>:A](p2 : Parser[B]) : Parser[B] = self.or(p, p2)
+    def **[B>:A](p2 : Parser[B]) : Parser[(A, B)] = self.product(p, p2)
+    def product[B>:A](p2 : Parser[B]) : Parser[(A, B)] = self.product(p, p2)
     def many : Parser[List[A]] = self.many(p)
+    def many1 : Parser[List[A]] = self.many1(p)
     def map[B](f : A => B) : Parser[B] = self.map(p)(f)
+    def slice[A] : Parser[String] = self.slice(p)
   }
 
   object Laws {
